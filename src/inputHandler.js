aee11e7b7c9b37a5b562192ab3bffe41aa5fea89
Jia Guo <jiaguo1024@gmail.com>
2017-09-23 09:16:15 -0500

    Handle swipe using Hammer.js

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -1,15 +1,27 @@
 /**
  * Created by Jia on 2017/9/23.
  */
+
+import Hammer from "hammerjs";
+
 class InputHandler {
 
     constructor(move) {
         this.move = move;
         this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleSwipe = this.handleSwipe.bind(this);
+        this.hammer = new Hammer(document);
+        this.hammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
     }
 
     registerListeners() {
         window.addEventListener("keyup", this.handleKeyDown)
+        this.hammer.on('swipeleft swiperight swipeup swipedown', this.handleSwipe);
+    }
+
+    removeListeners() {
+        window.removeEventListener("keydown", this.handleKeyDown);
+        this.hammer.off('swipeleft swiperight swipeup swipedown', this.handleSwipe)
     }
 
     handleKeyDown(event) {
@@ -32,8 +44,24 @@ class InputHandler {
         }
     }
 
-    removeListeners() {
-        window.removeEventListener("keydown", this.handleKeyDown);
+    handleSwipe(event) {
+        switch (event.type) {
+            case "swipeleft": {
+                this.move(0); break;
+            }
+            case "swipeup": {
+                this.move(1); break;
+            }
+            case "swiperight": {
+                this.move(2); break;
+            }
+            case "swipedown": {
+                this.move(3); break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 }
 
